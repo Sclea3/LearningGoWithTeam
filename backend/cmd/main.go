cee4712f30db455845b7727349e1efd86a0d5b9c
Sclea3 <sashamaty30@gmail.com>
2024-10-18 13:32:29 +0500

    1

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,4 +1,15 @@
 package main
 
 func main() {
+
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog
+
+	// TODO: init DB: sqlite
+
+	// TODO: init router: chi, "chi render"
+
+	// TODO: run server
+
 }
--- backend/internal/configs/configs.go
+++ backend/internal/configs/configs.go
@@ -1,1 +1,31 @@
 package configs
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Addr        string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoadConfig() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file %s does not exist", configPath)
+	}
+}
